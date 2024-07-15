9ec2f8e1051d17e902ea81b01c6a489581c017e1
Dan-tk <168034436+Dan-tk@users.noreply.github.com>
2024-07-15 18:29:41 +0300

    Update URL.ts

--- src/URL.ts
+++ src/URL.ts
@@ -1,2 +1,3 @@
 /*  export const URL = "http://localhost:8000"  */
- export const URL = "https://gr-8-api.onrender.com" 
+/*  export const URL = "https://gr-8-api.onrender.com"  */
+ export const URL = "https://meth.onrender.com"  
